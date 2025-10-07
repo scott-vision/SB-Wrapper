75652ced4bb2dd42df2a5f229b0c17d62218c492
Scott <scott_brooks1998@hotmail.co.uk>
2025-10-07 10:56:37 +0100

    Use unmodified Base modules for SBAccess

--- src/sbwrapper/csb_point.py
+++ src/sbwrapper/csb_point.py
@@ -1,10 +1,10 @@
-__copyright__  = "Copyright (c) 2022, Intelligent Imaging Innovations, Inc. All rights reserved.  All rights reserved."
-__license__  = "This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree."
+"""Compatibility wrapper around :mod:`CSBPoint`."""
 
-class CSBPoint(object):
-    """ generated source for class CSBPoint """
-    def __init__(self,val):
-        self.mX = val
-        self.mY = val
-        self.mZ = val
+from __future__ import annotations
+
+from ._base_loader import export_public, load_base_module
+
+_CSB_POINT = load_base_module("CSBPoint")
+
+__all__ = export_public(_CSB_POINT, globals())
 
